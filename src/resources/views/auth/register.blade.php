1fdb4305a5b41d2f04680895a1d3de332b38063e
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2022-05-12 11:53:21 +0200

    temp

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,8 @@
+@extends("layouts.app")
+
+@section("title", "Register")
+
+
+@section("content")
+<p>This is a register page</p>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+// // Authentication Routes...
+// Route::get('login', 'LoginController@showLoginForm')->name('login');
+// Route::post('login', 'LoginController@login');
+// Route::post('logout', 'LoginController@logout')->name('logout');
+
+// // Registration Routes...
+// Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+// Route::post('register', 'RegisterController@register');
