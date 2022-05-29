37f2eac3fe9dc566f5e4ca51a8be5bd5034186bf
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2022-05-29 19:08:28 +0200

    Frontend za /login

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,76 +5,57 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Login</div>
+    <div id="main" class="wrapper">
+        <div class="inner" style="max-width: 30em">
+            <h3>Prijava</h3>
+            <form id="form" method="POST" action="{{ route('login') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <div class=" row gtr-uniform">
+                    <div class="col-12">
+                        <input id="username" type="text"
+                            class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username"
+                            required autocomplete="username" placeholder="Korisničko ime">
 
-                        <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+                        @if ($errors->has('username'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('username') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                    <div class="col-12">
+                        <input id="password" type="password"
+                            class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                            required autocomplete="current-password" placeholder="Lozinka">
 
-                            <div class="col-md-6">
-                                <input id="username" type="username" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
 
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="current-password">
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <div class="col-12">
+                        <ul class="actions">
+                            <li>
+                                <button type="submit" class="primary">Prijavi se</button>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
 
 @section('scripts')
-  <script>
-    const params = new URLSearchParams(location.search);
-    if (params.get("banned")) {
-      showDialog({ content: "Vašem nalogu je zabranjen pristup.", type: "error" });
-    }
-  </script>
+    <script>
+        const params = new URLSearchParams(location.search);
+        if (params.get("banned")) {
+            showDialog({
+                content: "Vašem nalogu je zabranjen pristup.",
+                type: "error"
+            });
+        }
+    </script>
 @endsection
