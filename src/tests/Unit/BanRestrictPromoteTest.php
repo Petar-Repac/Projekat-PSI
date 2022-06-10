146492863542b1cbbb1d684198464ec5543bb7eb
Petar Repac <petar.repac@sbb.rs>
2022-06-10 21:16:40 +0200

    Završeni testovi za banovanje korisnika, zaključavnje objave i unapređenje korisnika

--- src/tests/Unit/BanRestrictPromoteTest.php
+++ src/tests/Unit/BanRestrictPromoteTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\Role;
+use App\Models\Post;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,27 +14,51 @@ class BanRestrictPromoteTest extends TestCase
     public function test_ban_user()
     {
         $admin = User::firstWhere('username', 'admin');
-        $user = User::firstWhere('username', 'Walter');
         $data = [
             "key" => "isBanned",
             "value" => true
         ];
         $this
             ->actingAs($admin)
-            ->patchJson("user/" . $user->username, $data);
+            ->patchJson("user/Walter", $data);
+
+
+        $user = User::firstWhere('username', 'Walter');
+
+        $this->assertEquals($user->isBanned, 1);
+    }
+
+    public function test_promote_user()
+    {
+        $admin = User::firstWhere('username', 'admin');
+        $data = [
+            "key" => "role",
+            "value" => "mod"
+        ];
         $this
             ->actingAs($admin)
-            ->post("logout");
+            ->patchJson("user/Asha20", $data);
 
+
+        $user = User::firstWhere('username', 'Asha20');
+
+        $this->assertEquals($user->role, Role::mod()->idRole);
+    }
+
+    public function test_restrict_post()
+    {
+        $admin = User::firstWhere('username', 'admin');
+        $post = Post::firstWhere('heading', 'Cyber Sex');
         $data = [
-            "username" => "Walter",
-            "password" => "tobago123"
+            "value" => 1
         ];
-
         $this
-            ->post("login", $data)
-            ->assertRedirect("/");
+            ->actingAs($admin)
+            ->patchJson("posts/" . $post->idPost, $data);
 
-        $this->assertNull(Auth::user());
+
+        $post = Post::firstWhere('heading', 'Cyber Sex');
+
+        $this->assertEquals($post->isLocked, 1);
     }
 }
