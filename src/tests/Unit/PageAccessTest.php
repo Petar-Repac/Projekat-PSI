295e14f80b3ad6d1bdb98eb37a3f3402cb098e19
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2022-06-10 10:53:44 +0200

    Dodaj PageAccessTest

---
+++ src/tests/Unit/PageAccessTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class PageAccessTest extends TestCase
+{
+    public function test_about_us_page_access()
+    {
+        $this->get('/about-us')->assertStatus(200);
+    }
+
+    public function test_login_page_access()
+    {
+        $this->get('/login')->assertStatus(200);
+    }
+
+    public function test_register_page_access()
+    {
+        $this->get('/register')->assertStatus(200);
+    }
+
+    public function test_home_page_access()
+    {
+        $this->get('/')->assertStatus(200);
+    }
+
+    public function test_post_page_access()
+    {
+        $this->get('/posts/1')->assertStatus(200);
+    }
+
+    public function test_user_page_access()
+    {
+        $this->get('/user/admin')->assertStatus(200);
+    }
+
+    public function test_writepost_page_redirects_guest()
+    {
+        $this->get('/writepost')->assertRedirect('/login');
+    }
+
+    public function test_writepost_page_accepts_auth_user()
+    {
+        $admin = User::where('username', 'admin')->first();
+        $this->actingAs($admin)->get('/writepost')->assertStatus(200);
+    }
+}
